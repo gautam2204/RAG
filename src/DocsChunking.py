00bafd653dd30bac74bcceab8e439c95a970854d
Gautam Rawat <Gautam.rawat123@gmail.com>
2026-01-28 18:50:48 +0530

    py pipeline creation

---
+++ src/DocsChunking.py
@@ -0,0 +1,21 @@
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from LoadData import DataIngestion
+
+
+class Chunking:
+    def split_documents(documents, chunk_size=1000, chunk_overlap=200):   
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=chunk_size,
+            chunk_overlap=chunk_overlap,
+        )
+
+        split_docs = text_splitter.split_documents(documents)
+        return split_docs
+    
+if __name__ == "__main__":
+    DataIngestionObj = DataIngestion(directory_path="./data")
+    documents = DataIngestionObj.get_documents()
+    chunked_documents = Chunking.split_documents(documents)
+    print(f"Number of chunked documents: {len(chunked_documents)}")
